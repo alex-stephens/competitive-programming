812a4c93b05f799b1a00550dd51c8e53ce10777f
alex-stephens <alexstephens9@gmail.com>
2018-02-03 18:11:03 +1100

    solved greedy algorithm p11900

---
+++ Greedy Algorithms/Non-Classical/p11900.py
@@ -0,0 +1,19 @@
+# Competitive Programming 3
+# Problem 11900
+
+T = int(input())
+
+for case in range(1,T+1):
+
+    n, P, Q = [int(x) for x in input().split()]
+    weights = sorted([int(x) for x in input().split()])
+    
+    num, total = 0, 0
+    for i in range(n):
+        if total + weights[i] <= Q and num + 1 <= P:
+            num += 1
+            total += weights[i]
+
+    print('Case ' + str(case) + ': ' + str(num) )
+    
+    
