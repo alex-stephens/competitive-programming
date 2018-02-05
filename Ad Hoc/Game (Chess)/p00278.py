1237e534a0fca7f93fd1dee4ea6ecd0cc2ec0943
RaviAlexStephens <alexstephens9@gmail.com>
2018-02-05 22:25:38 +1100

    solved ad hoc p00278 - Chess

--- Ad Hoc/Game (Chess)/p00278.py
+++ Ad Hoc/Game (Chess)/p00278.py
@@ -1,3 +1,17 @@
 # Competitive Programming 3
 # Problem 00278
 
+T = int(input())
+
+for _ in range(T):
+    inputStr = input().split()
+    piece, m, n = inputStr[0], int(inputStr[1]), int(inputStr[2])
+    
+    if piece == 'k':
+        print( (m*n + 1) // 2 )
+    
+    elif piece == 'r' or piece == 'Q':
+        print(min(m, n))
+        
+    elif piece == 'K':
+        print( ((n+1)//2) * ((m+1)//2))
