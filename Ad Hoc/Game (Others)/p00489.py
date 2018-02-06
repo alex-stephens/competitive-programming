5c1e2a0b503136e41ce764f142c29e5c3830f7ec
alex-stephens <alexstephens9@gmail.com>
2018-02-06 16:09:17 +1100

    solved ad hoc p00489 - Hangman Judge

---
+++ Ad Hoc/Game (Others)/p00489.py
@@ -0,0 +1,35 @@
+# Competitive Programming 3
+# Problem 00489
+
+while True:
+    R = int(input())
+    if R == -1: break
+
+    print('Round ' + str(R))
+    
+    target = set(input())
+    done, wrong = set(), 0
+    guesses  = input()
+    
+    verdict, msg = None, ['You win.', 'You lose.', 'You chickened out.']
+    
+    for g in guesses:
+        if g in done:
+            continue
+        else:
+            done.add(g)
+            
+        if g in target: target.remove(g)
+        else: wrong += 1
+            
+        if len(target) == 0: 
+            verdict = 0
+            break
+        elif wrong == 7: 
+            verdict = 1
+            break
+        
+    if verdict == None: verdict = 2
+    print(msg[verdict])
+        
+            
