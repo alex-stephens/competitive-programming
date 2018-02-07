5f86dc9d50d81e56a79ae3a3592a855c7073ed9a
RaviAlexStephens <alexstephens9@gmail.com>
2018-02-07 13:14:27 +1100

    attempted linear DS p11340 - Newspaper (TLE)

---
+++ 2.2 Linear DS/1D Array Manipulation/p11340.py
@@ -0,0 +1,27 @@
+# Competitive Programming 3
+# Problem 11340
+
+from time import time
+
+s = time()
+
+T = int(input())
+for _ in range(T):
+    K = int(input())
+    values = dict()
+    for _ in range(K):
+        line = input().split()
+        values[line[0]] = int(line[1])
+    #print(values)
+    
+    cents = 0
+    M = int(input())
+    for _ in range(M):
+        line = input()
+        for x in line:
+            if x in values:
+                cents += values[x]
+                
+    print('%.2f' % (cents/100),'$', sep='')
+
+print('time taken:', time() - s)
