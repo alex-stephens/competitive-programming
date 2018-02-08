62429a2158befa9f9e92623c6b83cae069e85c29
RaviAlexStephens <alexstephens9@gmail.com>
2018-02-08 23:03:39 +1100

    attempted dynamic programming 1D p507 - passes uDebug

--- 3.5 Dynamic Programming/Max 1D Range Sum/p507.py
+++ 3.5 Dynamic Programming/Max 1D Range Sum/p507.py
@@ -1,26 +1,47 @@
 # Competitive Programming 3
 # Problem 507
 
+T = int(input())
 
-while True:
-    try:
-        numbers = [int(x) for x in input().split()]
-    except EOFError:
-        break
+for t in range(1,T+1):
     
-    numbers.pop(-1)
-    length = len(numbers)
+    r = int(input())
     
-    lowest, highest = [0 for _ in range(length)], [0 for _ in range(length)]
-    lowest[0], highest[0] = numbers[0], numbers[0]
+    if r <= 1:
+        print("Route {} has no nice parts".format(t))
+        continue
     
-    for i in range(1, length):
-        if numbers[i] >= 0:
-            lowest[i] = min(lowest[i-1]*numbers[i], numbers[i])
-            highest[i] = max(highest[i-1]*numbers[i], numbers[i])
-        else:
-            lowest[i] = min(highest[i-1]*numbers[i], numbers[i])
-            highest[i] = max(lowest[i-1]*numbers[i], numbers[i])
+    niceness = [int(input()) for _ in range(r-1)]
+            
+    # score and length of best route ending at each index
+    score, length = [0 for _ in range(r-1)], [0 for _ in range(r-1)]
+    
+    score[0] = max(0, niceness[0])
+    length[0] = 1 if niceness[0] >= 0 else 0
+    
+    for i in range(1,r-1):
+        score[i] = max(score[i-1] + niceness[i], 0)
+        length[i] = length[i-1] + 1 if score[i] != 0 else 0
         
-    print(max(highest))
-        
+    bestScore = max(score)
+    solutions = score.count(bestScore)
+    bestSolIndex = score.index(max(score))
+    longest = length[bestSolIndex]
+    maxLength = 0
+    #print(score)
+    
+    for i in range(0,r-1):
+        if score[i] == bestScore and length[i] > longest:
+            longest = length[i]
+            bestSolIndex = i
+    
+    if bestScore > 0:
+        print("The nicest part of route {} is between stops {} and {}"\
+              .format(t, bestSolIndex - longest + 2, bestSolIndex+2))
+    else:
+        print("Route {} has no nice parts".format(t))
+                
+    
+    
+
+         
