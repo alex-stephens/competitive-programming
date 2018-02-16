0be014feccb6bdfb894fed665732e68e8f4429df
alex-stephens <alexstephens9@gmail.com>
2018-02-16 19:02:54 +1100

    solved linear DS p10107 - calculating the median

---
+++ 2.2 Linear DS/STL Algorithms/p10107.py
@@ -0,0 +1,24 @@
+# Competitive Programming 3
+# Problem 10107
+
+from bisect import bisect_left
+
+numbers = []
+length = 0
+
+while True:
+    try:
+        n = int(input())
+    except EOFError:
+        break
+    
+    length += 1
+    i = bisect_left(numbers, n)
+    numbers.insert(i, n)
+    
+    if length % 2 == 1:
+        print(numbers[length//2])
+    else:
+        print((numbers[length//2] + numbers[length//2 - 1])//2)
+    
+    
