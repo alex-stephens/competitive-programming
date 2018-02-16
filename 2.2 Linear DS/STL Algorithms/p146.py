30340c3662b2337ed1bcfc6018b5cf682fcfef2f
alex-stephens <alexstephens9@gmail.com>
2018-02-16 18:48:56 +1100

    solved linear DS p146 - next lexicographical permutation of a string

--- 2.2 Linear DS/STL Algorithms/p146.py
+++ 2.2 Linear DS/STL Algorithms/p146.py
@@ -1,3 +1,45 @@
 # Competitive Programming 3
 # Problem 146
 
+'''
+Function to compute next lexicographic permutation of a string
+'''
+def nextLex(A):
+    length = len(A)
+    found = False
+    for k in range(length-2, -1, -1):
+        if A[k] < A[k+1]:
+            found = True
+            break
+    
+    if not found: return A[::-1]
+    
+    for l in range(length-1, k, -1):
+        if A[l] > A[k]: break
+    
+    B = list(A)
+    
+    B[l], B[k] = B[k], B[l]
+    B[k+1:] = B[k+1:][::-1]
+    
+    return ''.join(B)
+    
+
+while True:
+    string = input()
+    if string == '#':
+        break
+    
+    new = nextLex(string)
+    if new[0] < string[0] or new == string:
+        print("No Successor")
+    else:
+        print(new)
+    
+    
+    
+    
+    
+    
+    
+    
