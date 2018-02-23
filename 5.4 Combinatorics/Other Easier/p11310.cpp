1e4b5f0fd617afc84c8207dfc08f9030bd20c5d8
alex-stephens <alexstephens9@gmail.com>
2018-02-23 14:39:11 +1100

    solved mathematical DP p11310 (C++)

---
+++ 5.4 Combinatorics/Other Easier/p11310.cpp
@@ -0,0 +1,28 @@
+// Competitive Programming 3
+// Problem 495
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define SIZE 50
+unsigned long long dp[SIZE];
+
+unsigned long long ways(int n){
+    if (dp[n] > 0) return dp[n];
+    else return dp[n] = 2*ways(n-3) + 4*ways(n-2) + ways(n-1);
+}
+
+int main(){
+    int T;
+    cin >> T;
+    memset(dp, 0, SIZE*sizeof(dp[0]));
+    dp[0] = 1, dp[1] = 1, dp[2] = 5;    // base cases
+
+    while (T--){
+        int n;
+        cin >> n;
+        cout << ways(n) << endl;
+    }
+    return 0;
+}
