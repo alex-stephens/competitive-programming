92b9a6660a6c35369dd5b63db990c09a19d3a3f4
RaviAlexStephens <alexstephens9@gmail.com>
2018-03-02 12:12:38 +1100

    solved palindrome p10945

---
+++ 1.4 Ad Hoc/Palindrome/p10945.py
@@ -0,0 +1,16 @@
+# Competitive Programming 3
+# Problem 10945
+
+while True:
+    string = input()
+    if string == "DONE": break
+    
+    string = string.lower()
+    newstring = ''.join([string[i] for i in range(len(string)) \
+                         if string[i] not in ' !?.,'])
+
+    
+    if newstring == newstring[::-1]:
+        print("You won't be eaten!")
+    else:
+        print("Uh oh..")
