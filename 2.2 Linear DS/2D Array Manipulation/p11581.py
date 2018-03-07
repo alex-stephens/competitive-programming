8a53114904df0061e0aee8e6cc9c6014e318a34b
alex-stephens <alexstephens9@gmail.com>
2018-03-07 15:36:44 +1100

    solved linear DS 2D grid p11581 - good ad hoc problem

--- 2.2 Linear DS/2D Array Manipulation/p11581.py
+++ 2.2 Linear DS/2D Array Manipulation/p11581.py
@@ -7,25 +7,39 @@ def f(g):
     new = [[0 for _ in range(3)] for _ in range(3)]
     for i in range(3):
         for j in range(3):
-            if i > 0: new[i][j] += grid[i-1][j]
-            if j > 0: new[i][j] += grid[i][j-1]
-            if i < 2: new[i][j] += grid[i+1][j]
-            if j < 2: new[i][j] += grid[i][j+1]
+            if i > 0: new[i][j] += g[i-1][j]
+            if j > 0: new[i][j] += g[i][j-1]
+            if i < 2: new[i][j] += g[i+1][j]
+            if j < 2: new[i][j] += g[i][j+1]
             new[i][j] %= 2
     return new
 
+# return interpretation of grid as binary value
+def val(g):
+    n = 2**8
+    ans = 0
+    for i in range(3):
+        for j in range(3):
+            ans += g[i][j] * n
+            n //= 2
+    return ans
+
 for _ in range(T):
     input()
-    grid = [[0 for _ in range(3)] for _ in range(3)]
+    g = [[0 for _ in range(3)] for _ in range(3)]
     for i in range(3):
         line = input()
         for j in range(3):
-            grid[i][j] = int(line[j])
+            g[i][j] = int(line[j])
             
-    for i in range(3):
-        print(grid[i])
-    new = f(grid)
-    for i in range(3):
-        print(new[i])
-    print('')
+    seen = dict()
+    count = -1 # initially have not shown i=0 is a solution
+                
+    while val(g) not in seen:
+        seen[val(g)] = 1
+        g = f(g)
+        count += 1
+
+    # subtract 1 to get the last index where it was finite
+    print(count-1)
             
